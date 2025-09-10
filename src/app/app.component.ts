7c5e6db7640241b8ed02d5e87a0916800b876a1c
Eugenio Viana <eugenioviana.developer@gmail.com>
2025-09-10 16:48:48 -0300

    adicionado rotas e toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button'
-
+import { MatToolbarModule } from '@angular/material/toolbar'
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , MatButtonModule],
+  imports: [RouterOutlet, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
