43e99caa4f9a365c6db8186b5f5aa31383c1b95b
Eugenio Viana <eugenioviana.developer@gmail.com>
2025-09-11 17:20:33 -0300

    criado service e repository

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout'
+import { MatCardModule } from '@angular/material/card'
+import { FormsModule } from '@angular/forms'
+import { MatInputModule} from '@angular/material/input'
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatIconModule} from '@angular/material/icon'
+import {MatButtonModule} from '@angular/material/button'
+import { Cliente } from './cliente';
+import { ClienteService } from '../cliente.service';
+;
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [FlexLayoutModule, 
+    MatCardModule, 
+    FormsModule,
+    MatInputModule, 
+    MatFormFieldModule,
+    MatButtonModule,
+  MatIconModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
+
+
+
+  cliente: Cliente = Cliente.newCliente();
+
+  constructor (private service: ClienteService){
+
+  }
+
+  salvar(){
+    this.service.salvar(this.cliente);
+  }
 
 }
